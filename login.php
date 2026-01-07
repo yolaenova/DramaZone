e2a40953c7b52ff8607e7c6bc70054cd037a5862
Yola Enova Sabilla <yolaenova@gmail.com>
2026-01-07 22:50:26 +0700

    ubah judul form login

--- login.php
+++ login.php
@@ -15,7 +15,7 @@ if (isset($_SESSION['username'])) {
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Login | DramaZone</title>
+    <title>Login |DramaZone</title>
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
       rel="stylesheet"
@@ -37,7 +37,7 @@ if (isset($_SESSION['username'])) {
                     <div class="card-body">
                         <div class="text-center mb-3">
                             <i class="bi bi-person-circle h1 display-4"></i>
-                            <p>DramaZone</p>
+                            <p>Walcome to DramaZone</p>
                             <hr />
                         </div>
                         <form action="" method="post" id="loginForm">
